4048acfc9194705b142cf19fe17da829bed320dc
Roman Lytvynenko <litvinenko95@gmail.com>
2020-01-22 22:07:51 +0200

    Updated embed to work with UTF links

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -143,7 +143,9 @@ class Response extends AbstractResponse
             list($mime, $charset) = array_map('trim', explode(';', $this->contentType));
 
             $this->contentType = $mime;
-            $this->content = Utils::toUtf8($content, substr(strstr($charset, '='), 1));
+
+            $charset = str_replace(['"', "'"], '', substr(strstr($charset, '='), 1));
+            $this->content = Utils::toUtf8($content, trim($charset));
         }
     }
 }
